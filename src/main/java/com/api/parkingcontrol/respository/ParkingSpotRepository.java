9f486c97e9e058412210ae37179f401fb20764a0
Fernando Ribeiro de Amorim <nandoamori@yahoo.com.br>
2023-04-24 14:03:10 -0300

    criando repository: ParkingSpotRepository

---
+++ src/main/java/com/api/parkingcontrol/respository/ParkingSpotRepository.java
@@ -0,0 +1,10 @@
+package com.api.parkingcontrol.respository;
+
+import com.api.parkingcontrol.model.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+    
+}
