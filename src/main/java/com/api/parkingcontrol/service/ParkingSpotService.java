b03bd13755568d31f66fd275eea2d9f18cd0e3f5
Fernando Ribeiro de Amorim <nandoamori@yahoo.com.br>
2023-04-24 14:11:33 -0300

    criando service: ParkSpotService

---
+++ src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
@@ -0,0 +1,17 @@
+package com.api.parkingcontrol.service;
+
+import com.api.parkingcontrol.respository.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+    @Autowired
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    
+}
